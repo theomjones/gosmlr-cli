64ad6d3f5d22421f33d9d8b1e480f81475740759
theomjones <theomjones@gmail.com>
2018-02-13 23:33:04 +0000

    fixed broken list feat, changed db store location

--- db/db.js
+++ db/db.js
@@ -1,8 +1,7 @@
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-const Url = require('./db')
 
-const adapter = new FileSync('./Data/urls.json')
+const adapter = new FileSync(__dirname + '/urls.json')
 const db = low(adapter)
 
 module.exports = db
