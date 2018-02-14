ff1f5b5fff62cea8b5d82976a95a163fbf85f6c1
theomjones <theomjones@gmail.com>
2018-02-14 12:56:10 +0000

    added timer function

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 
+const pjson = require('./package.json')
+console.log(pjson.version)
+
 const program = require('commander')
 const axios = require('axios')
 const ncp = require('copy-paste')
 const chalk = require('chalk')
 const urlController = require('./Controllers/UrlController')
+const emoji = require('node-emoji')
 
 const regex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm
+
+const apiUrl = process.env.API || 'https://gosmlr.xyz'
 
 program
     .option('-l --list', '<long_url> [--list] --- List your recent urls.', program)
@@ -15,23 +21,25 @@ program
 program
     .version('0.2.3')
     .action(url => {
-        if (url.search(regex) !== 0) {return console.log('\nERROR: ' + chalk.magenta(url) + chalk.red(' is not a valid url.'))}
-        axios.post('https://gosmlr.xyz', { url })
+        if (url.search(regex) !== 0) {return console.log('\nERROR: ' + chalk.magenta(url) + chalk.red(' is not a valid url. ') + emoji.get('no_good'))}
+        const processTime = process.hrtime()
+        axios.post(apiUrl, { url })
         .then(res => {
             return res.data
         })
         .then(json => {
             ncp.copy(json.shortUrl)
             urlController.add({ shortUrl: json.shortUrl, longUrl: json.longUrl })
-            console.log(`\n${chalk.green(json.shortUrl)} was copied to your clipboard! 🐶`)
-            console.log(chalk.blue(json.count) + ' urls have been made smaller!')
+            console.log(`\n${chalk.green(json.shortUrl)} was copied to your clipboard! ${emoji.get('dog')}`)
+            console.log('In total, ' + chalk.blue(json.count) + ' urls have been made smaller!')
+            console.log(chalk.yellow(`${Math.floor(process.hrtime(processTime)[1] / 1000000)}ms\n`))
             if (program.list) {
                 urlController.list()
             }
         })
         .catch((e) => {
             if (e.code === "ENOTFOUND") {
-                console.log(chalk.red("\nWe could not process your request.\n"))
+                console.log(chalk.red(`\nWe could not process your request. ${emoji.get("no_good")}\n`))
                 console.log(e.message)
             } else {
                 console.log(e)
